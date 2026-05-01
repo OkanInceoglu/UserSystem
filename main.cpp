cab823442a6ef64bf9eda65adc5cc9cea700e5ac
Okan Inceoglu <twovoss@gmail.com>
2026-05-01 21:27:54 +0300

    v2: add User class and CRUD system

--- main.cpp
+++ main.cpp
@@ -1,14 +1,65 @@
 ﻿#include <iostream>
+#include <vector>
 
 using namespace std;
+
+class User {
+private:
+    int id;
+    string name;
+    string email;
+
+public:
+    User(int userId, string userName, string userEmail) {
+        id = userId;
+        name = userName;
+        email = userEmail;
+    }
+
+    void display() {
+        cout << "ID: " << id
+            << " | Name: " << name
+            << " | Email: " << email << endl;
+    }
+};
+
+vector<User> users;
 
 void showMenu() {
     cout << "\n===== USER MANAGEMENT SYSTEM =====\n";
     cout << "1. Add User\n";
     cout << "2. List Users\n";
-    cout << "3. Delete User\n";
-    cout << "4. Exit\n";
-    cout << "Select an option: ";
+    cout << "3. Exit\n";
+    cout << "Select option: ";
+}
+
+void addUser() {
+    int id;
+    string name, email;
+
+    cout << "Enter ID: ";
+    cin >> id;
+
+    cout << "Enter Name: ";
+    cin >> name;
+
+    cout << "Enter Email: ";
+    cin >> email;
+
+    users.push_back(User(id, name, email));
+
+    cout << "User added successfully!\n";
+}
+
+void listUsers() {
+    if (users.empty()) {
+        cout << "No users found.\n";
+        return;
+    }
+
+    for (User u : users) {
+        u.display();
+    }
 }
 
 int main() {
@@ -20,25 +71,19 @@ int main() {
 
         switch (choice) {
         case 1:
-            cout << "Add User selected (not implemented yet)\n";
+            addUser();
             break;
 
         case 2:
-            cout << "List Users selected (not implemented yet)\n";
+            listUsers();
             break;
 
         case 3:
-            cout << "Delete User selected (not implemented yet)\n";
-            break;
-
-        case 4:
-            cout << "Exiting program...\n";
+            cout << "Exiting...\n";
             return 0;
 
         default:
             cout << "Invalid option!\n";
         }
     }
-
-    return 0;
 }
